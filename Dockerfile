d21a9503de7c75583e3a7d5e7cca2350bbc979f8
Vadim <v.marcenko@gmx.de>
2020-09-30 11:48:28 +0200

    Added brainscapes dependency

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,24 @@
+# Use python 3 as base image for the flask application
 FROM python:3
-COPY app/. /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-RUN pip install connexion[swagger-ui]
 
+# Upgrade pip to latest version
+RUN python -m pip install --upgrade pip
+
+# Install latest brainscapes client
+RUN pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple brainscapes
+
+# Copy the application and install dependencies
+COPY ./app /app
+WORKDIR /app
+RUN python -m pip install -r requirements.txt
+RUN python -m pip install connexion[swagger-ui]
+RUN python -m pip install anytree
+RUN python -m pip install pillow
+RUN python -m pip install scikit-image
+
+# Expose flask port
 EXPOSE 5000
 
+# Start flask application
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app/brainscapes_api.py
+++ app/brainscapes_api.py
@@ -1,26 +1,42 @@
+import json
+
+
+from brainscapes.atlas import REGISTRY
 from flask import request
 from brainscapes.authentication import Authentication
-from brainscapes.features import receptors
+
+
+def _set_auth_token():
+    auth = Authentication.instance()
+    auth.set_token(request.args['token'])
+
+
+def _create_atlas():
+    return REGISTRY.MULTILEVEL_HUMAN_ATLAS
+
+
+def query_data(modality, regionname, args=None):
+    _set_auth_token()
+    atlas = _create_atlas()
+    selected_region = atlas.regiontree.find(regionname)
+    atlas.select_region(selected_region[0])
+    return atlas.query_data(modality)
 
 
 def receptordata_fingerprint():
     if request.args and 'region' in request.args:
-        auth = Authentication.instance()
-        auth.set_token(request.args['token'])
-        receptor_data = receptors.get_receptor_data_by_region(request.args['region'])
-        return receptor_data.fingerprint.to_json()
+        receptor_data = query_data('ReceptorDistribution', request.args['region'])
+        return json.dumps(receptor_data[0].fingerprint)
     else:
         return "A region name must be provided as a query parameter", 400
 
 
 def receptordata_profiles():
     if request.args and 'region' in request.args:
-        auth = Authentication.instance()
-        auth.set_token(request.args['token'])
-        receptor_data = receptors.get_receptor_data_by_region(request.args['region'])
+        receptor_data = query_data('ReceptorDistribution', request.args['region'])
         data = {}
-        for key, profile in receptor_data.profiles.items():
-            data[key] = profile.to_json()
+        for key, profile in receptor_data[0].profiles.items():
+            data[key] = json.dumps(profile)
         return data
     else:
         return "A region name must be provided as a query parameter", 400
@@ -28,10 +44,78 @@ def receptordata_profiles():
 
 def receptordata_autoradiographs():
     if request.args and 'region' in request.args:
-        auth = Authentication.instance()
-        auth.set_token(request.args['token'])
-        receptor_data = receptors.get_receptor_data_by_region(request.args['region'])
-        return receptor_data.autoradiographs_files
+        receptor_data = query_data('ReceptorDistribution', request.args['region'])
+        data = {}
+        for key, autoradiographs in receptor_data[0].autoradiographs.items():
+            data[key] = 'PLI Image'
+        return data
     else:
         return "A region name must be provided as a query parameter", 400
+
+
+def parcellations():
+    atlas = _create_atlas()
+    parcellations = atlas.parcellations
+    result = []
+    for parcellation in parcellations:
+        result.append(json.dumps(parcellation.__dict__))
+    return result
+
+
+def spaces():
+    atlas = _create_atlas()
+    atlas_spaces = atlas.spaces
+    result = []
+    for space in atlas_spaces:
+        result.append(json.dumps(space.__dict__))
+    return result
+
+
+def _add_children_to_region(region_json, region):
+    for child in region.children:
+        o = {'name': child.name, 'children': []}
+        if child.children:
+            _add_children_to_region(o, child)
+        region_json['children'].append(o)
+
+
+def regions():
+    atlas = _create_atlas()
+    result = []
+    for region in atlas.regiontree.children:
+        region_json = {'name': region.name, 'children': []}
+        _add_children_to_region(region_json, region)
+        result.append(region_json)
+    return result
+
+
+def _find_space_by_id(atlas, space_id):
+    for space in atlas.spaces:
+        if space.id == space_id:
+            return space
+    return {}
+
+
+def maps():
+    atlas = _create_atlas()
+    space = _find_space_by_id(atlas, request.args['space'])
+    # return atlas.get_maps(space)
+    return {'result': 'Nifti data as json / binary or file url'}
+
+def templates():
+    atlas = _create_atlas()
+    space = _find_space_by_id(atlas, request.args['space'])
+    # return atlas.get_template(space)
+    return {'result': 'Nifti data as json / binary or file url'}
+
+
+def genes():
+    atlas = _create_atlas()
+    selected_region = atlas.regiontree.find(request.args['region'])
+    atlas.select_region(selected_region[0])
+    # genes_feature = atlas.query_data('GeneExpression', request.args['gene'])
+    genes_feature = atlas.query_data('GeneExpression', gene='c')
+    return genes_feature
+
+
 
