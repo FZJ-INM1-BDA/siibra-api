e2f7b178ad69d295c8b6919f452376a11cfd542d
Vadim <v.marcenko@gmx.de>
2020-10-28 09:23:25 +0100

    Added logging to matomo on production

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 from flask import (
-    render_template
+    render_template,
+    request
 )
 import connexion
+
+import requests
+
+import os
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='../')
@@ -16,9 +21,35 @@ def home():
     return render_template('index.html')
 
 
+@app.app.before_request
+def before_request_func():
+    test_url = request.url
+    headers = request.headers.get
+    test_list = ['.css', '.js', '.png', '.gif', '.json', '.ico']
+    res = any(ele in test_url for ele in test_list)
+
+    if 'BRAINSCAPES_ENVIRONMENT' in os.environ:
+        if os.environ['BRAINSCAPES_ENVIRONMENT'] == 'PRODUCTION':
+            print('******* Im on production ********')
+            if not res:
+                payload = {
+                        'idsite': 13,
+                        'rec': 1,
+                        'action_name': 'brainscapes_api',
+                        'url': request.url,
+                        '_id': 'my_ip',
+                        'lang':  request.headers.get('Accept-Language'),
+                        'ua': request.headers.get('User-Agent')
+                }
+                try:
+                    r = requests.get('https://stats.humanbrainproject.eu/matomo.php', params=payload)
+                    print('Matomo logging with status: {}'.format(r.status_code))
+                except:
+                    print('Could not log to matomo instance')
+        else:
+            print('Request for: {}'.format(request.url))
+
+
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
-
-
