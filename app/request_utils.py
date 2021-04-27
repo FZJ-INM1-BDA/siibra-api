72a6a78c4616fc61fa9c5f7f7fe786b74e403e09
Vadim <v.marcenko@gmx.de>
2021-04-27 09:33:53 +0200

    change the way of getting available parcellations in one space

--- app/request_utils.py
+++ app/request_utils.py
@@ -153,8 +153,9 @@ def get_spaces_for_parcellation(parcellation: str):
 def get_parcellations_for_space(space: str):
     result = []
     for p in bs.parcellations.items:
-        if bs.spaces[space].id in p.maps.keys():
-            result.append(_object_to_json(p))
+        for k in p.maps.keys():
+            if bs.spaces[space].id in k.id:
+                result.append(_object_to_json(p))
     return result
 
 
