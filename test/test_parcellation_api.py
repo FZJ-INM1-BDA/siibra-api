9870ac0fe1dc8d0e60f4b1d701b3760f70c1effb
Vadim <v.marcenko@gmx.de>
2021-04-27 09:03:21 +0200

    more robust assert for getting all parcellations

--- test/test_parcellation_api.py
+++ test/test_parcellation_api.py
@@ -21,7 +21,7 @@ def test_get_all_parcellations():
     )
     result_content = json.loads(response.content)
     assert response.status_code == 200
-    assert len(result_content) == 12
+    assert len(result_content) > 0
 
 
 def test_get_one_parcellation_by_id():
