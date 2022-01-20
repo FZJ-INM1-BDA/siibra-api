d9f8965275988b046d46ae08d2e892471959a584
Xiao Gui <xgui3783@gmail.com>
2022-01-20 13:33:38 +0100

    add colin 27 / availableParcellation to monitoring

--- e2e/test_space.py
+++ e2e/test_space.py
@@ -9,6 +9,7 @@ from .util import Session
 MULTILEVEL_HUMAN_ATLAS_ID='juelich/iav/atlas/v1.0.0/1'
 ICBM_152_SPACE_ID = 'minds/core/referencespace/v1.0.0/dafcffc5-4826-4bf1-8ff6-46b8a31ff8e2'
 BIGBRAIN_SPACE_ID = "minds/core/referencespace/v1.0.0/a1655b99-82f1-420f-a3c2-fe80fd4c8588"
+COLIN_27_SPACE_ID = "minds/core/referencespace/v1.0.0/7f39f7be-445b-47c0-9791-e971c0b6d992"
 INVALID_SPACE_ID = 'INVALID_SPACE_ID'
 
 base_url=os.getenv('SIIBRA_API_E2E_BASE_URL', 'http://localhost:5000')
@@ -17,6 +18,7 @@ client = Session(base_url=base_url)
 test_space_param = [
     (quote(MULTILEVEL_HUMAN_ATLAS_ID), quote_plus(ICBM_152_SPACE_ID), "MNI152 2009c nonl asym", 13),
     (quote(MULTILEVEL_HUMAN_ATLAS_ID), quote_plus(BIGBRAIN_SPACE_ID), "Big Brain", 4),
+    (quote(MULTILEVEL_HUMAN_ATLAS_ID), quote_plus(COLIN_27_SPACE_ID), "MNI Colin 27", 4),
 ]
 
 @pytest.mark.parametrize("atlas_id,space_id,ex_space_name,ex_avail_parc_len", test_space_param)
