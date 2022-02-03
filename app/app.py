ed6941881f7207d47169d4eabdb1ee6a7e6dcbaa
vadim <v.marcenko@fz-juelich.de>
2022-02-03 12:56:14 +0100

    Improve logging to matomo with further information

--- app/app.py
+++ app/app.py
@@ -18,6 +18,8 @@ import os
 import requests
 import json
 import siibra
+import uuid
+import hashlib
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.security import HTTPBearer
 from fastapi.templating import Jinja2Templates
@@ -34,6 +36,7 @@ from app.service.health import router as health_router
 from app.core.parcellation_api import router as parcellation_router
 from app.configuration.ebrains_token import get_public_token
 from app.configuration.siibra_custom_exception import SiibraCustomException
+from app.configuration.cache_redis import CacheRedis
 from . import logger
 from . import __version__
 import logging
@@ -62,7 +65,7 @@ tags_metadata = [
 ]
 
 ATLAS_PATH = '/atlases'
-siibra_version_header='x-siibra-api-version'
+siibra_version_header = 'x-siibra-api-version'
 
 # Main fastAPI application
 app = FastAPI(
@@ -146,72 +149,9 @@ def home(request: Request):
                             detail='Could not retrieve pypi statistics')
 
 
-@app.middleware('http')
-async def set_auth_header(request: Request, call_next):
-    """
-    Set authentication for further requests with siibra
-    If a user provides a header, this one will be used otherwise use the default public token
-    :param request: current request
-    :param call_next: next middleware function
-    :return: an altered response
-    """
-
-    path = request.scope['path']  # get the request route
-    print(f'path: {path}')
-
-    bearer_token = request.headers.get('Authorization')
-    try:
-        if bearer_token:
-            siibra.set_ebrains_token(bearer_token.replace('Bearer ', ''))
-        else:
-            public_token = get_public_token()
-            siibra.set_ebrains_token(public_token)
-        response = await call_next(request)
-        return response
-    except SiibraCustomException as err:
-        logger.error('Could not set authentication token')
-        return JSONResponse(
-            status_code=err.status_code, content={
-                'message': err.message})
-
-
-@app.middleware('http')
-async def matomo_request_log(request: Request, call_next):
-    """
-    Middleware will be executed before each request.
-    If the URL does not belog to a resource file, a log will be send to matomo monitoring.
-    :param request: current fastAPI request object
-    :param call_next: next middleware method
-    :return: the response after preprocessing
-    """
-    test_url = request.url
-    test_list = ['.css', '.js', '.png', '.gif', '.json', '.ico']
-    res = any(ele in str(test_url) for ele in test_list)
-
-    if 'SIIBRA_ENVIRONMENT' in os.environ:
-        if os.environ['SIIBRA_ENVIRONMENT'] == 'PRODUCTION':
-            if not res:
-                payload = {
-                    'idsite': 13,
-                    'rec': 1,
-                    'action_name': 'siibra_api',
-                    'url': request.url,
-                    '_id': 'my_ip',
-                    'lang': request.headers.get('Accept-Language'),
-                    'ua': request.headers.get('User-Agent')
-                }
-                try:
-                    # r = requests.get('https://stats.humanbrainproject.eu/matomo.php', params=payload)
-                    # print('Matomo logging with status: {}'.format(r.status_code))
-                    pass
-                except Exception:
-                    logger.info('Could not log to matomo instance')
-        else:
-            logger.info('Request for: {}'.format(request.url))
-    response = await call_next(request)
-    return response
-
-from app.configuration.cache_redis import CacheRedis
+# Each middleware function is called before the request is processed
+# For FastAPI the order is important.
+# The functions are called from bottom to top.
 
 async def read_bytes(generator) -> bytes:
     body = b""
@@ -222,7 +162,15 @@ async def read_bytes(generator) -> bytes:
 
 @app.middleware("http")
 async def cache_response(request: Request, call_next):
+    """
+    Cache requests to redis, to improve response time.
 
+    Cached content is returned with a new response. In this NO other following middleware will be called
+
+    :param request: current request
+    :param call_next: next middleware function
+    :return: current response or a new Response with cached content
+    """
     redis = CacheRedis.get_instance()
 
     cache_key = f"[{__version__}] {request.url.path}{str(request.url.query)}"
@@ -230,7 +178,7 @@ async def cache_response(request: Request, call_next):
     # bypass cache read if:
     # - method is not GET
     # - x-bypass-fast-api-cache is present
-    # - (NYI) if auth token is set 
+    # - (NYI) if auth token is set
     bypass_cache_read = request.method.upper() != "GET" or request.headers.get("x-bypass-fast-api-cache")
 
     # bypass cache set if:
@@ -249,7 +197,7 @@ async def cache_response(request: Request, call_next):
             "access-control-expose-headers": f"{siibra_version_header}",
             siibra_version_header: __version__,
         } if has_origin else {}
-        
+
         return Response(
             cached_value,
             headers={
@@ -275,10 +223,80 @@ async def cache_response(request: Request, call_next):
 
 
 @app.middleware('http')
+async def set_auth_header(request: Request, call_next):
+    """
+    Set authentication for further requests with siibra
+    If a user provides a header, this one will be used otherwise use the default public token
+    :param request: current request
+    :param call_next: next middleware function
+    :return: an altered response
+    """
+
+    bearer_token = request.headers.get('Authorization')
+    try:
+        if bearer_token:
+            siibra.set_ebrains_token(bearer_token.replace('Bearer ', ''))
+        else:
+            public_token = get_public_token()
+            siibra.set_ebrains_token(public_token)
+        response = await call_next(request)
+        return response
+    except SiibraCustomException as err:
+        logger.error('Could not set authentication token')
+        return JSONResponse(
+            status_code=err.status_code, content={
+                'message': err.message})
+
+
+@app.middleware('http')
 async def add_version_header(request: Request, call_next):
+    """
+    Add the latest application version as a custom header
+    """
     response = await call_next(request)
     response.headers[siibra_version_header] = __version__
     return response
+
+
+@app.middleware('http')
+async def matomo_request_log(request: Request, call_next):
+    """
+    Middleware will be executed before each request.
+    If the URL does not belong to a resource file, a log will be sent to matomo monitoring.
+    :param request: current fastAPI request object
+    :param call_next: next middleware method
+    :return: the response after preprocessing
+    """
+    test_url = request.url
+    test_list = ['.css', '.js', '.png', '.gif', '.json', '.ico', 'localhost']
+    res = any(ele in str(test_url) for ele in test_list)
+
+    if 'SIIBRA_ENVIRONMENT' in os.environ:
+        # if os.environ['SIIBRA_ENVIRONMENT'] == 'PRODUCTION':
+        if not res:
+            payload = {
+                'idsite': 13,
+                'rec': 1,
+                'action_name': 'siibra_api',
+                'url': request.url,
+                '_id': hashlib.blake2b(digest_size=8, key=request.client.host.encode()).hexdigest(),
+                'rand': str(uuid.uuid1()),
+                'lang': request.headers.get('Accept-Language'),
+                'ua': request.headers.get('User-Agent'),
+                '_cvar': {'1': ['environment', os.environ['SIIBRA_ENVIRONMENT']]}
+            }
+            print(payload)
+            try:
+                r = requests.get('https://stats.humanbrainproject.eu/matomo.php', params=payload)
+                print('Matomo logging with status: {}'.format(r.status_code))
+                pass
+            except Exception:
+                logger.info('Could not log to matomo instance')
+        else:
+            logger.info('Request for: {}'.format(request.url))
+    response = await call_next(request)
+    return response
+
 
 @app.get('/ready', include_in_schema=False)
 def get_ready():
