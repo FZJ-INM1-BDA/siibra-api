fd407a09f557c609b63e6eb03fec6b3eed0cc3d3
vadim <v.marcenko@fz-juelich.de>
2022-07-22 09:10:02 +0200

    restructure tests to use unittest TestCase

--- test/core/test_atlas_api.py
+++ test/core/test_atlas_api.py
@@ -1,31 +1,31 @@
 import json
+import unittest
 
 from fastapi.testclient import TestClient
-
 from app.app import app
 
 client = TestClient(app)
 
-ATLAS_ID = 'juelich/iav/atlas/v1.0.0/1'
 
+class TestAtlasApi(unittest.TestCase):
 
-def test_get_all_atlases():
-    response = client.get('/v2_0/atlases')
-    assert response.status_code == 200
-    result_content = json.loads(response.content)
-    assert len(result_content) == 4
+    ATLAS_ID = 'juelich/iav/atlas/v1.0.0/1'
 
+    def test_get_all_atlases(self):
+        response = client.get('/v2_0/atlases')
+        self.assertEuqal(response.status_code, 200)
+        result_content = json.loads(response.content)
+        self.assertEuqal(len(result_content), 4)
 
-def test_get_singe_atlas():
-    response = client.get('/v2_0/atlases/{}'.format(ATLAS_ID.replace('/', '%2F')))
-    assert response.status_code == 200
-    result_content = json.loads(response.content)
-    assert result_content['@id'] == ATLAS_ID
+    def test_get_singe_atlas(self):
+        response = client.get('/v2_0/atlases/{}'.format(self.ATLAS_ID.replace('/', '%2F')))
+        self.assertEuqal(response.status_code, 200)
+        result_content = json.loads(response.content)
+        self.assertEuqal(result_content['@id'], self.ATLAS_ID)
 
-
-def test_get_invalid_atlas():
-    invalid_id = 'INVALID_ID'
-    response = client.get('/v2_0/atlases/{}'.format(invalid_id.replace('/', '%2F')))
-    assert response.status_code == 400
-    result_content = json.loads(response.content)
-    assert result_content['detail'] == 'atlas_id: {} is not known'.format(invalid_id)
+    def test_get_invalid_atlas(self):
+        invalid_id = 'INVALID_ID'
+        response = client.get('/v2_0/atlases/{}'.format(invalid_id.replace('/', '%2F')))
+        self.assertEuqal(response.status_code, 400)
+        result_content = json.loads(response.content)
+        self.assertEuqal(result_content['detail'], 'atlas_id: {} is not known'.format(invalid_id))
