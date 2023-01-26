de724951ebbd5cf2e8194923fcdf0ef783ddaa34
Xiao Gui <xgui3783@gmail.com>
2023-01-26 17:33:36 +0100

    use python 3.10 for tests remove bla

--- bla.py
+++
@@ -1,7 +0,0 @@
-
-from api.serialization.util import instance_to_model
-from api.serialization.util.siibra import StreamlineCounts
-
-feat, *_ = StreamlineCounts.get_instances()
-
-instance_to_model(feat)
