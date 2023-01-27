e3d5de2b849aa75b4b611fefa2ad0db94a63e342
Xiao Gui <xgui3783@gmail.com>
2023-01-27 14:54:17 +0100

    fix worker requirement fix api config

--- api/siibra_api_config.py
+++ api/siibra_api_config.py
@@ -6,9 +6,13 @@ ROLE = os.environ.get("SIIBRA_API_ROLE", "all")
 
 CELERY_CHANNEL = os.environ.get("SIIBRA_API_CELERY_CHANNEL", f"siibra-api-{__version__}")
 
+REDIS_HOST = "redis" or os.getenv("SIIBRA_REDIS_SERVICE_HOST") or os.getenv("REDIS_SERVICE_HOST") or os.getenv("REDIS_HOST") or "localhost"
+REDIS_PORT = os.getenv("SIIBRA_REDIS_SERVICE_PORT") or os.getenv("REDIS_SERVICE_PORT") or os.getenv("REDIS_PORT") or 6379
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
 class CELERY_CONFIG:
-    broker_url=os.getenv("SIIBRA_API_CELERY_BROKER", "redis://127.0.0.1:6379")
-    result_backend=os.getenv("SIIBRA_API_CELERY_RESULT", "redis://127.0.0.1:6379")
+    broker_url=os.getenv("SIIBRA_API_CELERY_BROKER", f"redis://{(':' + REDIS_PASSWORD + '@') if REDIS_PASSWORD else ''}{REDIS_HOST}:{REDIS_PORT}")
+    result_backend=os.getenv("SIIBRA_API_CELERY_RESULT", f"redis://{(':' + REDIS_PASSWORD + '@') if REDIS_PASSWORD else ''}{REDIS_HOST}:{REDIS_PORT}")
 
     worker_send_task_events = True
     task_send_sent_event = True
@@ -16,9 +20,5 @@ class CELERY_CONFIG:
     include=['api.common.data_handlers', 'api.serialization']
 
 LOGGER_DIR = os.environ.get("SIIBRA_API_LOG_DIR")
-
-REDIS_HOST = "redis" or os.getenv("SIIBRA_REDIS_SERVICE_HOST") or os.getenv("REDIS_SERVICE_HOST") or os.getenv("REDIS_HOST") or "localhost"
-REDIS_PORT = os.getenv("SIIBRA_REDIS_SERVICE_PORT") or os.getenv("REDIS_SERVICE_PORT") or os.getenv("REDIS_PORT") or 6379
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 IS_CI = os.getenv("CI") is not None
