e639a35fd0ad7933b62764967f6cb6c2f399687d
Xiao Gui <xgui3783@gmail.com>
2023-03-20 16:22:55 +0100

    fix: siibra anchor location prop can be point

--- api/models/features/anchor.py
+++ api/models/features/anchor.py
@@ -1,6 +1,9 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 from api.models._commons import ConfigBaseModel
 from api.models.locations.location import LocationModel
+from api.models.locations.point import (
+    CoordinatePointModel,
+)
 from api.models.core.region import ParcellationEntityVersionModel
 
 class SiibraRegionAssignmentQual(ConfigBaseModel):
@@ -8,5 +11,5 @@ class SiibraRegionAssignmentQual(ConfigBaseModel):
     qualification: str
 
 class SiibraAnchorModel(ConfigBaseModel, type="anchor"):
-    location: Optional[LocationModel]
+    location: Optional[Union[LocationModel, CoordinatePointModel]]
     regions: List[SiibraRegionAssignmentQual]
