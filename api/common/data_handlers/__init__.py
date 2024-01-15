669bce6c8aa3db46d8277f6b514991cc30af9f77
Xiao Gui <xgui3783@gmail.com>
2024-01-15 13:34:38 +0100

    fix top level import

--- api/common/data_handlers/__init__.py
+++ api/common/data_handlers/__init__.py
@@ -3,7 +3,7 @@ from . import features
 from . import volumes
 from . import compounds
 
-from ...siibra_api_config import ROLE
+from api.siibra_api_config import ROLE
 
 if ROLE == "all" or ROLE == "worker":
     import siibra
