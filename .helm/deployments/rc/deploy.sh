6f4fb2d9774b349f6d47aeba2e6705ddf3934c1f
Xiao Gui <xgui3783@gmail.com>
2025-02-17 14:39:20 +0100

    fix: deploy rc scripts

--- .helm/deployments/rc/deploy.sh
+++ .helm/deployments/rc/deploy.sh
@@ -10,31 +10,47 @@ do
     HELM_STATUS=$?
     
     helm_path=""
+    spec=""
     if [[ "$file" == *"server"* ]]
     then
         helm_path=.helm/siibra-api-v4-server/
+        spec=$DOCKER_IMGSHA_SERVER
     fi
 
     if [[ "$file" == *"worker"* ]]
     then
         helm_path=.helm/siibra-api-v4-worker/
+        spec="$DOCKER_IMGSHA_WORKER"
+        if [[ "$file" == *"worker-v4.yaml" ]]
+        then
+            spec="$DOCKER_IMGSHA_WORKER_V4"
+        fi
     fi
 
-    if [[ $helm_path == "" ]]
+    if [[ "$helm_path" == "" ]]
     then
         echo "$file does not match to any, skipping"
         continue
+    fi
+    
+    if [[ "$spec" == "" ]]
+    then
+        echo "spec not set. Something went wrong."
+        exit 1
     fi
 
     if [[ "$HELM_STATUS" == "0" ]]
     then
         echo "upgrading $prefix$file ..."
         helm upgrade -f $f \
+            --set spec=$spec \
             --history-max 3 \
             $prefix$file \
             $helm_path
     else
         echo "[NEW] installing $prefix$file ..."
-        helm install -f $f $prefix$file $helm_path
+        helm install -f $f \
+            --set spec=$spec \
+            $prefix$file $helm_path
     fi
 done
